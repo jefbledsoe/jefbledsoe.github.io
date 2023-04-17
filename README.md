e52a581b4a8450d289f757a23bdc8b205024baa5
jefbledsoe <jefbleds@gamil.com>
2023-04-17 13:28:10 -0400

    removed old files added the new ones from local

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# jefbledsoe.github.io
+Sub Reqs
+1 home page
+2 css file for github pages
+3 the file where you import bootstrap for extra credit
+4 PacMen Exercise Repository
+5 Eye Exercise Repository
+6 Real Time Bus Tracking Repository
+
+2 min intro video w/ overview, skills and fun facts should be an embeded youtube video
+each repository should have good REAMME files
---
+++ logic.js
@@ -0,0 +1,40 @@
+window.onload = function updateScreen() {
+    var backgroundImage = document.getElementById('bgImage');
+  var screenWidth = window.innerWidth;
+  var carousel = document.getElementById("carouselExampleCaptions");
+  var card = document.getElementById("myCard");
+  var prevbutton = document.getElementById("prevbutton");
+  var nextbutton = document.getElementById("nextbutton");
+
+    console.log('screen width: ' + screenWidth);
+    console.log('screen height: ' + window.innerHeight);
+
+
+  if (screenWidth <= 500) {
+    // do somthing
+    
+    card.style.left = "50%";
+    card.style.width = "60%";
+    card.style.height = "20%";
+    
+    card.style.translate = "10%";
+    carousel.style.position = "relative";
+    carousel.style.width = "80%";
+    carousel.style.translate = "10% 20%";
+
+  } else if (screenWidth <= 1200) {
+    carousel.style.position = "absolute";
+    carousel.style.width = "50%";
+    carousel.style.translate = "50%";
+    carousel.style.top = "65px";
+
+  } else {
+    card.style.position = "relative";
+    card.style.top = "65px";
+    carousel.style.position = "absolute";
+    carousel.style.width = "50%";
+    carousel.style.height = "80%";
+    carousel.style.translate = "50%";
+    carousel.style.top = "65px";
+  }
+};
