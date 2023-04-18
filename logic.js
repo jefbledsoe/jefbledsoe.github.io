e191205416948888ee635f79746a163da2739938
jefbledsoe <jefbleds@gamil.com>
2023-04-18 00:12:55 -0400

    bootstrap updates

--- logic.js
+++ logic.js
@@ -1,48 +1,8 @@
-// window.onload = function updateScreen() {
-//     var backgroundImage = document.getElementById('bgImage');
-//   var screenWidth = window.innerWidth;
-//   var carousel = document.getElementById("carouselExampleCaptions");
-//   var card = document.getElementById("myCard");
-//   var skillsCard = document.getElementById("skillsCard");
-//   var prevbutton = document.getElementById("prevbutton");
-//   var nextbutton = document.getElementById("nextbutton");
+var carousel = document.getElementById('carouselExampleCaptions');
+var row2 = document.getElementById('row2');
+var row3 = document.getElementById('row3');
+console.log(window.innerWidth);
 
-//     console.log('screen width: ' + screenWidth);
-//     console.log('screen height: ' + window.innerHeight);
-
-
-//   if (screenWidth <= 500) {
-//     // do somthing
-    
-
-
-//     card.style.left = "50%";
-//     card.style.width = "60%";
-//     card.style.height = "20%";
-    
-//     card.style.translate = "10%";
-//     carousel.style.position = "relative";
-//     carousel.style.width = "80%";
-//     carousel.style.translate = "10% 20%";
-
-//   } else if (screenWidth <= 1200) {
-//     carousel.style.position = "absolute";
-//     carousel.style.width = "50%";
-//     carousel.style.translate = "50%";
-//     carousel.style.top = "65px";
-
-//   } else {
-//     card.style.position = "relative";
-//     card.style.top = "65px";
-
-//     skillsCard.style.position = "absolute";
-//     skillsCard.style.left = "1000px";
-//     skillscard.style.top = "65px";
-
-//     carousel.style.position = "absolute";
-//     carousel.style.width = "50%";
-//     carousel.style.height = "80%";
-//     carousel.style.translate = "50%";
-//     carousel.style.top = "65px";
-//   }
-// };
+if(window.innerWidth<=100){
+  
+}
